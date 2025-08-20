da7d15a8a5db078a1a791614867ad702e75cdfba
Avilarrealr <149917560+Avilarrealr@users.noreply.github.com>
2025-08-20 23:32:41 +0000

    Adding app.js and index.html files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  // Boton de reload page
+  const reload = document.getElementById("reload");
+  reload.addEventListener("click", (_) => {
+    location.reload();
+  });
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = genDomainName()
 };
+
+let genDomainName = () => {
+
+  let pronoun = ['the', 'our', 'we', 'their', 'our']
+  let adj = ['great', 'big', 'chepeast', 'colorfull']
+  let noun = ['jogger', 'racoon', 'doggy', 'company', 'snake']
+  let dot = ['.es', '.com', '.net', '.io', '.us']
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length)
+  let adjIndex = Math.floor(Math.random() * adj.length)
+  let nounIndex = Math.floor(Math.random() * noun.length)
+  let dotIndex = Math.floor(Math.random() * dot.length)
+
+  return pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] +  dot[dotIndex]
+}
