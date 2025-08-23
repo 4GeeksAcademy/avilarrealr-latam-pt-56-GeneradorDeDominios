05dd12d509218cc61b64874c89cb53bcc4e43371
Avilarrealr <149917560+Avilarrealr@users.noreply.github.com>
2025-08-23 00:38:37 +0000

    Last changes in app.js for loop

--- src/app.js
+++ src/app.js
@@ -5,27 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   // Boton de reload page
   const reload = document.getElementById("reload");
   reload.addEventListener("click", (_) => {
     location.reload();
   });
   //write your code here
-  document.querySelector("#domain").innerHTML = genDomainName()
-};
-
-let genDomainName = () => {
-
+  
   let pronoun = ['the', 'our', 'we', 'their', 'our']
   let adj = ['great', 'big', 'chepeast', 'colorfull']
-  let noun = ['jogger', 'racoon', 'doggy', 'company', 'snake']
-  let dot = ['.es', '.com', '.net', '.io', '.us']
+  let noun = ['jogger', 'racoon', 'doggy', 'compus', 'snake', 'columpio']
+  let dot = ['es', 'com', 'net', 'io', 'us']
 
-  let pronounIndex = Math.floor(Math.random() * pronoun.length)
-  let adjIndex = Math.floor(Math.random() * adj.length)
-  let nounIndex = Math.floor(Math.random() * noun.length)
-  let dotIndex = Math.floor(Math.random() * dot.length)
+  let domainNames = []
+  for (let elemPronoun of pronoun) {
+    for (let elemAdj of adj) {
+      for (let elemNoun of noun) {
+        for (let elemDot of dot) {
+           domainNames.push(`${elemPronoun}${elemAdj}${elemNoun}.${elemDot}`)
+        }
+      }
+    }
+  }
 
-  return pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] +  dot[dotIndex]
-}
+  document.querySelector("#domain").innerHTML = domainNames[Math.floor(Math.random() * domainNames.length)]
+};
+
+  // if ((noun[nounIndex]).slice(2) == dot[dotIndex]) {
+  //   return pronoun[pronounIndex] + adj[adjIndex] + (noun[nounIndex]).slice(-2) + '.' + dot[dotIndex]
+  // }
+
